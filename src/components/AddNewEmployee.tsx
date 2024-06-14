647f4cc70883146530fbe3cde0470b776dffc0a4
Pritha <prithasen006@gmail.com>
2024-06-14 17:43:51 +0530

    used react hook form for adding new employee.

--- src/components/AddNewEmployee.tsx
+++ src/components/AddNewEmployee.tsx
@@ -1,69 +1,53 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  city: string;
+  project: string;
+  isActive: string;
+}
 
 export default function AddNewEmployee() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    city: '',
-    project: '',
-    isActive: 'Yes',
-  });
-
-  const [errors, setErrors] = useState({
-    firstName: false,
-    city: false,
-    project: false,
+  const { register, handleSubmit, control, formState: { errors } } = useForm<FormData>({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      city: '',
+      project: '',
+      isActive: 'Yes',
+    },
   });
 
   const navigate = useNavigate();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const onSubmit = async (data: FormData) => {
+    try {
+      const response = await fetch('https://664207cf3d66a67b3435e466.mockapi.io/api/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...data,
+          isActive: data.isActive === 'Yes',
+        }),
+      });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const newErrors = {
-      firstName: !formData.firstName,
-      city: !formData.city,
-      project: !formData.project,
-    };
-
-    setErrors(newErrors);
-
-    if (!newErrors.firstName && !newErrors.city && !newErrors.project) {
-      try {
-        const response = await fetch('https://664207cf3d66a67b3435e466.mockapi.io/api/v1/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            firstName: formData.firstName,
-            lastName: formData.lastName,
-            city: formData.city,
-            project: formData.project,
-            isActive: formData.isActive === 'Yes',
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        navigate('/');
-      } catch (error) {
-        console.error('Error saving the employee data', error);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+
+      navigate('/');
+    } catch (error) {
+      console.error('Error saving the employee data', error);
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#0B2447]">
-      <form onSubmit={handleSubmit} className="p-6 bg-[#A5D7E8] shadow rounded-lg w-full max-w-lg mt">
+      <form onSubmit={handleSubmit(onSubmit)} className="p-6 bg-[#A5D7E8] shadow rounded-lg w-full max-w-lg mt-10">
         <h2 className="text-lg font-semibold leading-7 text-gray-900 mb-6 text-center">Add New Employee</h2>
         <div className="grid grid-cols-1 gap-6">
           <div>
@@ -72,10 +56,8 @@ export default function AddNewEmployee() {
             </label>
             <input
               type="text"
-              name="firstName"
               id="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
+              {...register('firstName', { required: 'First name is required' })}
               autoComplete="given-name"
               className={`mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${
                 errors.firstName ? 'ring-red-500' : 'ring-gray-300'
@@ -83,7 +65,7 @@ export default function AddNewEmployee() {
                 errors.firstName ? 'focus:ring-red-500' : 'focus:ring-indigo-600'
               } sm:text-sm sm:leading-6`}
             />
-            {errors.firstName && <p className="text-red-500 text-xs mt-1">First name is required</p>}
+            {errors.firstName && <p className="text-red-500 text-xs mt-1">{errors.firstName.message}</p>}
           </div>
 
           <div>
@@ -92,10 +74,8 @@ export default function AddNewEmployee() {
             </label>
             <input
               type="text"
-              name="lastName"
               id="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
+              {...register('lastName')}
               autoComplete="family-name"
               className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
@@ -107,10 +87,8 @@ export default function AddNewEmployee() {
             </label>
             <input
               type="text"
-              name="city"
               id="city"
-              value={formData.city}
-              onChange={handleChange}
+              {...register('city', { required: 'City is required' })}
               autoComplete="address-level2"
               className={`mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${
                 errors.city ? 'ring-red-500' : 'ring-gray-300'
@@ -118,7 +96,7 @@ export default function AddNewEmployee() {
                 errors.city ? 'focus:ring-red-500' : 'focus:ring-indigo-600'
               } sm:text-sm sm:leading-6`}
             />
-            {errors.city && <p className="text-red-500 text-xs mt-1">City is required</p>}
+            {errors.city && <p className="text-red-500 text-xs mt-1">{errors.city.message}</p>}
           </div>
 
           <div>
@@ -127,10 +105,8 @@ export default function AddNewEmployee() {
             </label>
             <input
               type="text"
-              name="project"
               id="project"
-              value={formData.project}
-              onChange={handleChange}
+              {...register('project', { required: 'Project is required' })}
               autoComplete="project-name"
               className={`mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${
                 errors.project ? 'ring-red-500' : 'ring-gray-300'
@@ -138,23 +114,27 @@ export default function AddNewEmployee() {
                 errors.project ? 'focus:ring-red-500' : 'focus:ring-indigo-600'
               } sm:text-sm sm:leading-6`}
             />
-            {errors.project && <p className="text-red-500 text-xs mt-1">Project is required</p>}
+            {errors.project && <p className="text-red-500 text-xs mt-1">{errors.project.message}</p>}
           </div>
 
           <div>
             <label htmlFor="isActive" className="block text-sm font-medium leading-6 text-gray-900">
               Active Status
             </label>
-            <select
-              id="isActive"
+            <Controller
               name="isActive"
-              value={formData.isActive}
-              onChange={handleChange}
-              className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-            >
-              <option>Yes</option>
-              <option>No</option>
-            </select>
+              control={control}
+              render={({ field }) => (
+                <select
+                  id="isActive"
+                  {...field}
+                  className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>Yes</option>
+                  <option>No</option>
+                </select>
+              )}
+            />
           </div>
         </div>
 
