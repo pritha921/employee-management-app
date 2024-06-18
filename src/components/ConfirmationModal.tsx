9b17bf1e8f5b1cbe82ef4a71a8a0721fcc2d4f8f
Pritha Sen <prithasen006@gmail.com>
2024-06-18 18:07:29 +0530

    changed the text of modal's button.

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -28,7 +28,7 @@ const ConfirmationModal = ({
             onClick={onConfirm}
             className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
           >
-            Sure
+            Delete
           </button>
         </div>
       </div>
