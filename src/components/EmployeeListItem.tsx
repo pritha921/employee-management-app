d3970993ee480ca68f40e027cf367aa874761c97
Pritha Sen <prithasen006@gmail.com>
2024-06-18 18:03:37 +0530

    Fixed the issue of alignment of the labels.

--- src/components/EmployeeListItem.tsx
+++ src/components/EmployeeListItem.tsx
@@ -51,25 +51,27 @@ const EmployeeListItem = ({
         <p className="text-sm text-gray-500">{project}</p>
         <p className="text-sm text-gray-500">{city}</p>
       </div>
-      <div className="flex items-center space-x-6">
-        <div className="flex flex-col items-center space-y-2">
+      <div className="flex flex-col space-y-4">
+        <div className="flex items-center justify-between">
           <p className="text-sm font-semibold text-gray-900">Status</p>
-          <Switch
-            checked={isActive}
-            onChange={() => onToggleActive(id, !isActive)}
-            className={`relative inline-flex h-5 w-10 items-center rounded-full transition-colors duration-200 ease-in-out ${
-              isActive ? "bg-[#0A6847]" : "bg-[#C80036]"
-            }`}
-          >
-            <span
-              className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-200 ease-in-out ${
-                isActive ? "translate-x-6" : "translate-x-1"
-              }`}
-            />
-          </Switch>
-        </div>
-        <div className="flex flex-col items-center space-y-2">
           <p className="text-sm font-semibold text-gray-900">Action</p>
+        </div>
+        <div className="flex justify-between items-center space-x-6">
+          <div className="flex items-center">
+            <Switch
+              checked={isActive}
+              onChange={() => onToggleActive(id, !isActive)}
+              className={`relative inline-flex h-5 w-10 items-center rounded-full transition-colors duration-200 ease-in-out ${
+                isActive ? "bg-[#0A6847]" : "bg-[#C80036]"
+              }`}
+            >
+              <span
+                className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-200 ease-in-out ${
+                  isActive ? "translate-x-6" : "translate-x-1"
+                }`}
+              />
+            </Switch>
+          </div>
           <div className="flex flex-row items-center space-x-2">
             <button
               onClick={handleEditClick}
